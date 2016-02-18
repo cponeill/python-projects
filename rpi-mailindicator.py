b7ddce34c3e437e86ac11e4965bcceb717f8881b
brownsnake <me@browntech.tk>
2016-02-18 07:39:58 -0800

    Update rpi-mailindicator.py

--- rpi-mailindicator.py
+++ rpi-mailindicator.py
@@ -2,6 +2,8 @@
 #This is a work-in-progress script that can be customized
 #Intended for use on a Raspberry Pi 2 board with a red LED connected at pin GPIO 2, yellow LED at pin GPIO 3, and green LED at pin
 #GPIO 4. However, you can use arguments to specify other BCM mode pin numbers
+#Also, please be sane and use resistors on your LEDs. Make sure that the ANODE (+, positive) lead is directly connected to
+#desired output pin, and that the CATHODE (-, negative) lead is connected to a ground pin with a resistor between them.
 
 #The main purpose of this script is to create an LED indicator bar to tell you if you have unread email in your GMail inbox.
 #Red light means you have mail in the Important category
@@ -14,6 +16,7 @@
 
 import sys, imaplib
 import RPi.GPIO as g
+from time import sleep
 
 red_pin, yellow_pin, green_pin = 2, 3, 4
 
@@ -31,4 +34,34 @@ for i in sys.argv:
   if sys.argv[i] == "-r": red_pin = int(sys.argv[i+1])
   if sys.argv[i] == "-y": yellow_pin = int(sys.argv[i+1])
   if sys.argv[i] == "-g": green_pin = int(sys.argv[i+1])
-  
+
+#Setup the GPIO header as needed/desired  
+g.setmode(g.BCM)
+g.setup(red_pin, g.OUT, pull_up_down=g.PUD_DOWN)
+g.setup(yellow_pin, g.OUT, pull_up_down=g.PUD_DOWN)
+g.setup(green_pin, g.OUT, pull_up_down=g.PUD_DOWN)
+g.output(red_pin, 0)
+g.output(yellow_pin, 0)
+g.output(green_pin, 0)
+
+#Now, let's get signed into G-Mail...
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login(USERNAME, PASSWORD)
+
+#Okay, here is the loop that checks for unseen email in different folders/categories. Because this is meant to be an
+#"always on" type script, there will be a sleep at the end to prevent busy waiting and over-use of the processor.
+#Also, I suggest running this script with a trailing & or in a screen instance so that you can still use your terminal
+while True:
+  mail.select('inbox')
+  green_count = mail.search(None,'UnSeen')
+  mail.select('finances') #Change this to whatever category you want for the yellow light
+  yellow_count = mail.search(None, 'UnSeen')
+  mail.select('important') #Change this whatever category you want for the red light
+  red_count = mail.search(None, 'UnSeen')
+  if red_count == 0: g.output(red_pin, 0)
+  else: g.output(red_pin, 1)
+  if yellow_count == 0: g.output(yellow_pin, 0)
+  else: g.output(yellow_pin, 1)
+  if green_count == 0: g.output(green_pin, 0)
+  else: g.output(green_pin, 1)
+  sleep(0.5)
