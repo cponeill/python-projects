0e49190d7c1a8da78a6a24ec1ee6d3b1eba8585d
brownsnake <me@browntech.tk>
2016-02-19 11:44:14 -0800

    Update euler9.py

--- euler9.py
+++ euler9.py
@@ -1,5 +1,7 @@
 #Copyright 2015
 #This script was written to solve problem 9 on Project Euler
+#The objective here was to find a Pythagorean triple (three numbers where a^2+b^2=c^2) whose sum was 1000.
+#I decided to go with a brute-force method that tried basically every combination of numbers chosen at random until something fit
 
 import random
 import math
