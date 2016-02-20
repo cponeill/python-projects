02a20d92c3fa8a76e0c8d5411e56ca75b25606c2
brownsnake <me@browntech.tk>
2016-02-19 21:30:11 -0800

    Update euler10.py
    updated with correct goal of 2 million, not 1.

--- euler10.py
+++ euler10.py
@@ -1,12 +1,11 @@
 #Copyright 2016
 #This script is written to solve Problem 10 on Project Euler
-#My goal is to find the sum of all primes below 1 million
+#My goal is to find the sum of all primes below 2 million
 
 from math import sqrt
 
-final = 17
-for i in range(8,1000000): #Yes, I know that this will not include 1M in the loop. However, I also know that 1M isn't prime, so it doesn't matter
-    if i % 2 == 0: continue
+final = 17 #All Primes below 10 add to 17
+for i in range(11,2000000,2): #Yes, I know that this will not include 2M in the loop. However, I also know that 2M isn't prime, so it doesn't matter
     limit = int(sqrt(i)) + 1
     prime = 1
     for j in range(2,limit):
